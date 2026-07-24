3a7257ecab61ce2325b39cb51d570a06c6a96918
Mir Elias Hashimi <elias.h.989@hotmail.com>
2026-07-24 21:16:36 +0200

    changed index.html script.js db.js

---
+++ script.js
@@ -0,0 +1,32 @@
+// übergreifende function die beim laden ausgeführt wird
+function init() {
+
+}
+
+// Funktion die die Kategorien ausführt
+function renderCategories() {
+
+}
+
+// was muss in renderCategories gerendert werden
+// woher kommt der inhalt 
+// wann und wie lange
+// wann und wo wird der inhalt gespeichert setItemToLocalStorage
+// wann und wo wird der inhalt abgerufen getItemFromLocalStorage
+
+// function für das hinzufügen der gerichte in den Warenkorb
+// wann, wie und wohin wird ein Gericht hinzugefügt
+// was wird übergeben durch die function
+
+// function zum entfernen
+
+// function zum löschen der gerichte 
+// wann und wo werden gerichte gelöscht
+
+// functtion zum berechnen der Preise
+// wo und wann wird der Preis berechnet
+
+// function für das berechnen des gesamt preises
+// was wird dazu gerechnet oder abgerechnet
+
+// 
---
+++ scripts/db.js
@@ -0,0 +1,95 @@
+let menuCategories = [
+    {
+        "Category": "Burger & Sandwiches",
+        "dishes": [
+            {
+                "image": "",
+                "name": "Veggie mushroom black burger",
+                "insied": "Mixed green salad, Tomatoes, Edamame, Mushrooms",
+                "price": "16,90",
+                "add": "+"
+            },
+            {
+                "image": "",
+                "name": "All meat burger",
+                "insied": "Beef, Bacon, Dill pickles, Smoked cheese, Ketchup, BBQ souse",
+                "price": "15,90",
+                "add": "+"
+            },
+            {
+                "image": "",
+                "name": "Beef red burger",
+                "insied": "Beef, Cheese, Tomatoes, Lettuce, Onion",
+                "price": "14,90",
+                "add": "+"
+            },
+            {
+                "image": "",
+                "name": "Big chicken burger",
+                "insied": "Chicken, Cheese, Tomatoes, Lettuce, Onion, Bell pepper",
+                "price": "15,90",
+                "add": "+"
+            },
+        ]   
+    },
+    {
+        "Category": "Pizza",
+        "dishes": [
+            {
+                "image": "",
+                "name": "Pizza Margherita",
+                "insied": "Tomato Sauce, Mozzarella",
+                "price": "11,90",
+                "add": "+"
+            },
+            {
+                "name": "Pizza Chorizo",
+                "insied": "Tomato slices, Mozzarella, Chorizo",
+                "price": "13,90",
+                "add": "+"
+            },
+            {
+                "name": "Funghi",
+                "insied": "Red onion, Olives, Button Mushrooms, Mozzarella",
+                "price": "12,90",
+                "add": "+"
+            },
+            {
+                "name": "Quattro Formaggi with Chicken",
+                "insied": "Chicken, Mozzarella, Gorgonzola, Fontina,  Parmigiano Reggiano",
+                "price": "15,90",
+                "add": "+"
+            },
+        ]   
+    },
+    {
+        "Category": "Salad",
+        "dishes": [
+            {
+                "image": "",
+                "name": "Warm beef arugula salad",
+                "insied": "Beef, Arugula, Field salad, Greek feta, Cherry tomatoes, Sun-dried Tomatoes, Balsamic-vinegar dressing",
+                "price": "16,90",
+                "add": "+"
+            },
+            {
+                "name": "Mini green Salad",
+                "insied": "Green salad, Cucumber, Carrots, Parsley, Radishes ",
+                "price": "7,90",
+                "add": "+"
+            },
+            {
+                "name": "Green Salad with sea food",
+                "insied": "Mixed greens, Cherry tomatoes, Red onion, Mussels, Squid rings, Shrimp, Dijon mustard-lemon dressing with dill",
+                "price": "16,90",
+                "add": "+"
+            },
+            {
+                "name": "Vegan green salad with tofu",
+                "insied": "Green salad, Cherry tomatoes, Cucumber, Baby spinach, Edamame, Radishes, Bittercress, Tofu, Peanuts",
+                "price": "14,90",
+                "add": "+"
+            },
+        ]   
+    }
+]
